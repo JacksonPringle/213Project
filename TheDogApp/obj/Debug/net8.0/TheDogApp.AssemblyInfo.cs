945013cc32d2bebebdb03e3c502c2a21cf284408
Jackson Pringle <jackson.pringle@ndus.edu>
2024-12-15 22:18:37 -0600

    Fix naming error

--- TheDogApp/obj/Debug/net8.0/TheDogApp.AssemblyInfo.cs
+++ TheDogApp/obj/Debug/net8.0/TheDogApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheDogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed034e0673aa475852134b0c33b2d237a709508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d17c9854bbe36bacaa8ec84bfd10bfc982ad29ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheDogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheDogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
