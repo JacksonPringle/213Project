305cccc8faf8c43572f5f0478dbd3a50b22ac920
mlizz <elizabeth.reed.3@ndsu.edu>
2024-12-15 22:24:28 -0600

    added user and personal user classes

---
+++ TheDogApp/PersonalUser.cs
@@ -0,0 +1,14 @@
+﻿namespace TheDogApp.Components
+{
+    public class PersonalUser : User
+    {
+        public string? Phone { get; set; }
+        public List<int> HeartedDogs { get; set; }
+        public PersonalUser(int id, string user, string? email, string psswd,
+            string? phone, List<int> heartedDogs) : base(id, user, email, psswd)
+        {
+            Phone = phone;
+            HeartedDogs = heartedDogs;
+        }
+    }
+}
---
+++ TheDogApp/User.cs
@@ -0,0 +1,18 @@
+﻿namespace TheDogApp.Components
+{
+    public class User
+    {
+        private int ID { get; init; }
+        private string Username { get; set; }
+        private string? Email { get; set; }
+        private string Password { get; set; }
+
+        public User(int id, string user, string? email, string psswd)
+        {
+            ID = id;
+            Username = user;
+            Email = email;
+            Password = psswd;
+        }
+    }
+}
