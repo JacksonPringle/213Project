f50402ee2298fe7284fc7080440ca08513f96972
Adam-Bitz <adam.bitz@ndsu.edu>
2024-12-18 03:34:47 -0600

    Added Delete event functionality and admin permissions to Event view

--- TheDogApp/Message.cs
+++ TheDogApp/Message.cs
@@ -2,16 +2,15 @@
 {
     public class Message
     {
-        private static int _nextId = 1; // static property for Id count (can be replaced by database integration at some point)
         public int MessageId {  get; set; }
         public int ConversationId { get; set; }
         public string SenderId {  get; set; }
         public string Text { get; set; }
         public DateTime Timestamp { get; set; }
         public bool IsRead { get; set; }
-        public Message(int conversationId, string senderId, string text)
+        public Message(int messageId, int conversationId, string senderId, string text)
         {
-            MessageId = _nextId++; // set messageid to the next available value
+            MessageId = messageId;
             ConversationId = conversationId;
             SenderId = senderId;
             Text = text;
